6639e03d90fc099e0f7a064a51c793b581a4dd87
ramz <ramelopanisjr.06@gmail.com>
2024-11-29 04:25:19 +0800

    test

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,7 +1,7 @@
 // src/config.js
 
 const config = {
-    API_URL: import.meta.env.VITE_API_URL || "http://localhost:5000", // Default to localhost if not defined
+    API_URL: import.meta.env.VITE_API_URL || "http://3.107.195.194:5000", // Default to localhost if not defined
   };
   
   export default config;
