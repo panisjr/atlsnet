c37234136492c51e0de06668ff5286151d5facc3
ramz <ramelopanisjr.06@gmail.com>
2024-12-01 12:08:33 +0800

    adding logout modal in violation records

--- frontend/src/screens/ViolationRecord.jsx
+++ frontend/src/screens/ViolationRecord.jsx
@@ -265,53 +265,7 @@ const ViolationRecord = () => {
         )}
       </div>
       {/* Logout Modal */}
-      <div
-        className="modal fade"
-        id="logoutBackdrop"
-        data-bs-backdrop="static"
-        data-bs-keyboard="false"
-        tabIndex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <div className="d-flex align-items-center justify-content-center">
-                <div className="logo"></div>
-                <h4 className="mt-3">
-                  <strong>ATLS</strong>
-                </h4>
-              </div>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              <p>Are you sure you want to logout?</p>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={logout}
-                  className="btn btn-danger"
-                  data-bs-dismiss="modal"
-                >
-                  Yes
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LogoutModal logout={logout} />
       {/* Delete  Modal */}
       <div
         className="modal fade"
