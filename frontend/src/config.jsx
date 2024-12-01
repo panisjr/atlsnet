7278ea0273e653ce6cac32126d9195d12ee566f1
ramz <ramelopanisjr.06@gmail.com>
2024-12-01 11:59:26 +0800

    updates

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -2,7 +2,7 @@
 
 const config = {
     API_URL: import.meta.env.VITE_API_URL || "https://atlsnetserver.site", // Default to localhost if not defined
-    // API_URL: "http://localhost:5000", // Default to localhost if not defined
+    // API_URL: "http://localhost:5000", // Default to localhost if not defined 
   };
   
   export default config;
