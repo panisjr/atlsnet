32981ff61a4815ca8fd44027cc0bb49e54947712
Ubuntu <ubuntu@ip-172-31-11-226.ap-southeast-2.compute.internal>
2024-12-01 04:03:22 +0000

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ cd $PROJECT_DIR
 # Pulling latest changes from the dev branch
 echo "Pulling latest changes from the dev branch..."
 # Use SSH for authentication
-git remote set-url origin git@github.com:panisjr/atlsnet.git
+#git remote set-url origin git@github.com:panisjr/atlsnet.git
 git checkout main
 git pull origin main
-git push origin main
+#git push origin main
 #git reset --hard origin/dev
 
 # Activate the virtual environment
